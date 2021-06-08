dd8ba3f20614e714409bf559cd8bbc1ddc01eeaf
Hussein Sarea <zzzsssx0@gmail.com>
2021-06-08 21:46:39 +0300

    commit workingwithStrings

---
+++ intro/workingWithStrings.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $name = "Hussein Sarea";
+      echo substr($name , 0 ,7) . "<br>"; # Hussein
+      echo strtolower($name) . "<br>";
+      echo strtoupper($name);
+     ?>
+  </body>
+</html>
