c3ade8ee9ad63b9f86e5a7f39aa2aad030b839a4
Hussein Sarea <zzzsssx0@gmail.com>
2021-06-09 18:29:51 +0300

    commit

--- intro/variables.php
+++ intro/variables.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <?php
-      $characterName = 'Hussein Sarea'; # string variable
-      $characterAge = 22; # integer variable
-      echo "<p>There once was a man named $characterName </p>";
-      echo "<p>He was $characterAge years old </p>";
-      $characterName = "Rema";
-      echo "<p>He really liked the name ". substr($characterName ,0 , 7). "</p>"; # use . to concatenate two strings
-      echo "<p>But didn't like being $characterAge <p>";
-     ?>
-  </body>
-</html>
+<?php
+  //Declaring variablrs
+  $name = "Hussein Sarea";
+
+ ?>
