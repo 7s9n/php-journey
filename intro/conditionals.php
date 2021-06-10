28eac961c11c8b59cd0d93f86834db0fe3de6f4d
Hussein Sarea <zzzsssx0@gmail.com>
2021-06-10 04:52:09 +0300

    commit

--- intro/conditionals.php
+++ intro/conditionals.php
@@ -22,5 +22,43 @@
   $age == '20'; //true
 
   $age === 20; // true
-  $age === '20' //false
+  $age === '20'; //false
+
+  //if and / &&
+  if ($age <= 20 and $salary == 300000) {
+    echo "2 condition are true";
+  }
+  // if OR / ||
+  if ($age > 20 or $salary === 300000) {
+    echo "string";
+  }
+  // Ternary if
+  echo $age < 22 ? 'Young' : 'Old';
+  echo '<br>';
+
+  // Short ternary
+  $myAge = $age ?: 18; // Equivalent of "$age ? $age : 18"
+
+  // Null coalescing operator
+  $var = isset($name) ? $name : 'John';
+  $var = $name ?? 'John'; // Equivalent of above
+  echo $var.'<br>';
+
+  // switch
+  $userRole = 'admin'; // admin, editor, user
+
+  switch ($userRole) {
+      case 'admin':
+          echo 'You can do anything<br>';
+          break;
+      case 'editor';
+          echo 'You can edit content<br>';
+          break;
+      case 'user':
+          echo 'You can view posts and comment<br>';
+          break;
+      default:
+          echo 'Unknown role<br>';
+  }
+
  ?>
